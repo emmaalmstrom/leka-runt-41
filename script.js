34c3d44dce853f1e68bd247ce5aa9312c29351b9
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-08 12:34:48 +0200

    knsdlcnw

---
+++ script.js
@@ -0,0 +1,13 @@
+const knapp = document.getElementById("knapp1")
+const knappp = document.getElementById("knapp2")
+
+knapp.onclick = klick
+
+function klick(){
+    alert("Hoppas du mår bra!")
+}
+knappp.onclick = klikk
+
+function klikk(){
+    alert("Ska jag sätta på te till dig?")
+}
